d19f3474b442fe792db03e8ac10046a0db3f8867
Manar Mohammed Abo alkhair <mkmanar762@gmail.com>
2017-04-16 00:22:32 -0700

    Add files via upload

---
+++ E12.java
@@ -0,0 +1,16 @@
+
+
+
+import java.util.Random;
+
+
+public class E12 {
+
+    
+    public static void main(String[] args) {
+       Random genertor =new Random();
+       int i =genertor.nextInt(6)+1;
+       System.out.println(i);
+    }
+    
+}
